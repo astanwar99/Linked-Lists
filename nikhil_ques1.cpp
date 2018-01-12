76f378df9d006cf9eabe6b769e1ec13dc57c9369
Ashwani Singh Tanwar <30383121+astanwar99@users.noreply.github.com>
2018-01-13 00:05:31 +0530

    Add files via upload

---
+++ nikhil_ques1.cpp
@@ -0,0 +1,116 @@
+/*
+Nikhil Chauhan
+17BCE0610
+
+Question 1:
+Q1. Create a stack and write a code to reverse the elements of a stack
+using only stack operations (push and pop).
+
+*/
+
+
+#include<iostream>
+
+using namespace std;
+
+//Stack declaration
+struct stack
+{
+    int a[20];
+    int top;
+}st;
+
+//Push function definition
+void push(int val)
+{
+    if (st.top==19)
+    {
+        cout<<"\nStack overflow\n";
+    }
+    else
+    {
+        st.a[++st.top]=val;
+    }
+}
+
+//Pop function definition
+int pop()
+{
+    if(st.top>=0)
+        return st.a[st.top--];
+    else
+        return 0;
+}
+
+//Reverse function for reversing the stack
+
+void reverse_stack()
+{
+    int temp[20],i,counter=0;        //temp is a temporary array to store stack data
+    for(i=st.top;i>=0;i--)           //for loop for moving data from stack to temp
+    {
+        temp[i]=pop();
+        counter++;
+    }
+    for(i=counter-1;i>=0;i--)        //for loop for moving data from temp to stack
+    {                                //but in reverse order.
+        push(temp[i]);
+    }
+
+
+}
+
+//Display function for displaying stack content
+void display()
+{
+    cout<<"\ntop->"<<st.a[st.top]<<"\n";
+    for(int i=st.top-1;i>=0;i--)
+    {
+        cout<<"     "<<st.a[i]<<"\n";
+    }
+}
+
+//Main function-optimized for menu driven program
+int main()
+{
+    st.top=-1;
+    int choice,num,x; //x is a temporary variable for storing popped data.
+    cout<<"================================\n"
+        <<"Choose an option.\n"
+        <<"1.Push element\n"
+        <<"2.Pop element\n"
+        <<"3.Reverse stack\n"
+        <<"4.Display Stack\n"
+        <<"5.Exit\n"
+        <<"================================\n";
+    do
+    {
+        cout<<"Choice: ";
+        cin>>choice;
+        switch(choice)
+        {
+        case 1:
+            cout<<"\nEnter element:";
+            cin>>num;
+            push(num);
+            break;
+        case 2:
+            x=pop();
+            cout<<x<<" popped\n";
+            break;
+        case 3:
+            reverse_stack();
+            break;
+        case 4:
+            display();
+            break;
+        case 5:
+            break;
+        default:
+            cout<<"\nWrong choice";
+
+        }
+    }while(choice!=5);
+    return 0;
+
+}
---
+++ nikhil_ques4.cpp
@@ -0,0 +1,134 @@
+/*
+Nikhil Chauhan
+17BCE0610
+
+Q4. Write a code to convert an infix expression to its equivalent revers polish expression
+*/
+
+#include<iostream>
+#include<string>
+#define size 20
+using namespace std;
+
+char infix[20],postfix[20];
+
+//Structure definition for stack
+struct stack
+{
+	int s[size];
+	int top;
+}st;
+
+//Push function for stack
+void push(int ele)
+{
+	st.top++;
+	st.s[st.top]=ele;
+}
+
+/*
+Pop function for stack. It also returns the character
+which is popped.
+Each operator has been assigned a numerical value
+depending upon its precedence.
+*/
+char pop()
+{
+	int el;
+	char e;
+	el=st.s[st.top];
+	st.top--;
+	switch(el)
+	{
+		case 1:e='+';
+				break;
+		case 2:e='-';
+				break;
+		case 3:e='*';
+				break;
+		case 4:e='/';
+				break;
+		case 5:e='^';
+				break;
+	}
+	return(e);
+}
+
+/*
+Post function for carrying out the conversion from infix
+to Reverse Polish Expression(Postfix).
+*/
+
+void post()
+{
+	int i,j=0;
+	for(i=0;infix[i]!='\0';i++)
+	{
+		switch(infix[i])
+		{
+			case '+':
+				{
+					while(st.s[st.top]>=1)
+						postfix[j++]=pop();
+					push(1);
+					break;
+				}
+			case '-':
+				{
+					while(st.s[st.top]>=1)
+						postfix[j++]=pop();
+					push(2);
+					break;
+				}
+			case '*':
+				{
+					while(st.s[st.top]>=3)
+						postfix[j++]=pop();
+					push(3);
+					break;
+				}
+			case '/':
+				{
+					while(st.s[st.top]>=3)
+						postfix[j++]=pop();
+					push(4);
+					break;
+				}
+			case '^':
+				{
+					while(st.s[st.top]>=5)
+						postfix[j++]=pop();
+					push(5);
+					break;
+				}
+			case '(':
+				{
+					push(0);
+					break;
+				}
+			case ')':
+				{
+					while(st.s[st.top]!=0)
+						postfix[j++]=pop();
+					st.top--;
+					break;
+				}
+			default :postfix[j++]=infix[i];
+		}
+	}
+	while(st.top>0)     //For moving left out operators from stack to postfix array.
+	postfix[j++]=pop();
+	cout<<"\nThe postfix expression is: ";
+	cout<<postfix;
+	cout<<"\n";
+}
+
+int main()
+{
+	st.top=0;
+    cout<<"\nEnter the expression: ";
+    cin>>infix;
+    cout<<"Evaluating using Reverse Polish notation:\n";
+    post();
+    return 0;
+}
